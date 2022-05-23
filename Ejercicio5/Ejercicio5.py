4a6675c846c6b550e1f39449e33d5898227145a9
Juan <105739344+contra3000@users.noreply.github.com>
2022-05-23 11:52:38 -0300

    PyEx5 Openbootcamp
    Se sube capturas de consola con versión de código.

---
+++ Ejercicio5/Ejercicio5.py
@@ -0,0 +1,20 @@
+
+Nombre = "Marcelo"
+Apellidos = "Guevara Guerrico"
+Edad = 35
+Email = "mgg@gmail.com"
+Telefono = "+54381-6139852"
+Casado = False
+conHijos = False
+friendList = ["Mario", "Pedro", "José", "Ignacio", "Gerardo"]
+seenMovies = {1: "Rapido y Furioso", 2: "Shrek", 3: "Armagedon", 4: "La bella y la bestia"}
+
+print(Nombre)
+print(Apellidos)
+print(Edad)
+print(Email)
+print(Telefono)
+print(Casado)
+print(conHijos)
+print(friendList)
+print(seenMovies)
